ec25003aac270f2222ab097c40fb3839f360b142
p.baluk <pawel.marcin.baluk@gmail.com>
2019-06-25 23:13:04 +0200

    config heroku

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/todo-list'));
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/todo-list/index.html'));
-});
-
-// default Heroku port
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
